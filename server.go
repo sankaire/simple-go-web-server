24fced26928ecb71da381d95461404e4c9964fab
serahshani <kerenketepela@gmail.com>
2022-01-15 13:01:02 +0300

    initial commit

---
+++ server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+var counter int
+var mutex = &sync.Mutex{}
+
+func echoString(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello Kenya")
+}
+
+func incrementCounter(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	counter++
+	fmt.Fprintf(w, strconv.Itoa(counter))
+	mutex.Unlock()
+}
+
+func main() {
+	http.HandleFunc("/", echoString)
+	http.HandleFunc("/increment", incrementCounter)
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hi")
+	})
+	log.Fatal(http.ListenAndServe(":8001", nil))
+}
