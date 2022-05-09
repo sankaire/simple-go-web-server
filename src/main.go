fdf040933ab6317adb166b1cc5f650c8f27b6af2
Carver <kerenketepela@gmail.com>
2022-05-09 10:02:36 +0300

    move to files to src

---
+++ src/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type Todo struct {
+	Title string
+	Done  bool
+}
+
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
+}
+
+func main() {
+	tmpl := template.Must(template.ParseFiles("layout.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Task 1", Done: false},
+				{Title: "Task 2", Done: true},
+				{Title: "Task 3", Done: true},
+			},
+		}
+		tmpl.Execute(w, data)
+	})
+	fmt.Print("app running on 8000")
+	http.ListenAndServe(":8000", nil)
+}
